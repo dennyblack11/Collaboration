e2d5efbf5eeab1f46c53da6676ca77d2e6d2b47d
dennyblack11 <dennisozoemena3@gmail.com>
2023-11-10 15:22:29 +0100

    process

--- index.ts
+++ index.ts
@@ -7,3 +7,17 @@ const port: number = 3400;
 const Server = app.listen(port, () => {
     console.log("Server is live on pport", port)
 })
+
+process.on("uncaughtException", (error) => {
+    console.log("uncaughtException")
+
+    process.exit(1)
+})
+
+process.on("unhandledRejection", (reason) => {
+    console.log("Server is shutting down due to an unhandled rejection", reason);
+
+    Server.close(() => {
+        process.exit(1)
+    })
+})
